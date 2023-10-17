6fe61b31dfa40829741bb278fd46515d30db3a84
brianmorgan12 <142330882+brianmorgan12@users.noreply.github.com>
2023-10-17 10:36:20 -0600

    Add files via upload

---
+++ funciones.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+ int suma (int, int);
+ int main(){
+ 	
+ 	int num1, num2, sum;
+ 	printf("Dame 2 numeros a sumar\n");
+ 	scanf("%d %d", &num1, &num2);
+ 	
+  sum = num1 + num2;
+  printf("la suma es: %d", sum);
+  
+ 	
+ return 0;		
+ }
+ 
+ int suma (int a, int b)
+ 
+ {
+ 	int add;
+ 	add = a + b 
+ 	return add;
+ 	
+ }
